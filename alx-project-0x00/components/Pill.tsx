b8f50bae9f62967ca7b2202823c7111aa4b2d9d5
Mary <maryauma2606@gmail.com>
2025-06-01 20:51:19 +0300

    add initials in component

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,14 @@
-const Pill: React.FC = () => {
+import { PillProps } from "@/interfaces";
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
     </div>
-  )
-}
+  );
+};
+
+export default Pill;
+
 
 export default Pill;
